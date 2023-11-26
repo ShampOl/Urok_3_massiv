eccb7bdf5b4a907f998a9fa4838d2f6063033193
Olya <lolcap202@gmail.com>
2023-11-26 17:37:51 +0600

    add zadanie1

---
+++ zad1/Program.cs
@@ -0,0 +1,32 @@
+﻿  {
+        // Создаем одномерный массив из 10 целых чисел от 1 до 100
+        int[] array = new int[10];
+        Random random = new Random();
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = random.Next(1, 101);
+        }
+
+        // Выводим массив для наглядности
+        Console.WriteLine("Массив: " + string.Join(", ", array));
+
+        // Находим количество элементов в отрезке [20, 90]
+        int countInRange = CountElementsInRange(array, 20, 90);
+
+        // Выводим результат
+        Console.WriteLine("Количество элементов в отрезке [20, 90]: " + countInRange);
+    }
+
+    static int CountElementsInRange(int[] array, int lowerBound, int upperBound)
+    {
+        int count = 0;
+        foreach (int num in array)
+        {
+            if (num >= lowerBound && num <= upperBound)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
